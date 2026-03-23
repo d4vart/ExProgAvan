60cb5d4bd2f67cefc3331a616ba59686127e9464
Arthur <arthurpratestorres.atp@gmail.com>
2026-03-23 19:57:10 -0300

    Tarefa Model

---
+++ ListaEx/src/main/java/com/atp/ListaEx/model/TarefaModel.java
@@ -0,0 +1,53 @@
+package com.atp.ListaEx.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity(name = "Tarefa")
+@Table(name = "tb_tarefa")
+public class TarefaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "descricao_tarefa")
+    private String descricao;
+
+    @Column(name = "data_vencimento")
+    private LocalDate dataVencimento;
+
+    private Boolean concluida;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Boolean getConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(Boolean concluida) {
+        this.concluida = concluida;
+    }
+}
