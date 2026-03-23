0d0d52b015366535c82544e38eb9dab5f7c3c05b
Arthur <arthurpratestorres.atp@gmail.com>
2026-03-23 18:42:45 -0300

    Pedido Repository

---
+++ ListaEx/src/main/java/com/atp/ListaEx/repository/PedidoRepository.java
@@ -0,0 +1,7 @@
+package com.atp.ListaEx.repository;
+
+import com.atp.ListaEx.model.PedidoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<PedidoModel, Long>{
+}
